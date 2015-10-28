a65b90fa37da22b4b75509b4f7379accffab32ee
Chris <christopherwebb@Christophers-MacBook-Pro-2.local>
2015-10-27 23:01:04 -0500

    Cleaning Up
    Making the functions separate so that they aren’t in one big file.

--- stepwise_backwards.R
+++ stepwise_backwards.R
@@ -1,13 +1,3 @@
-# Truncate a dataset with censoring indicators to change the end time
-truncateSurvivalTime <- function(event_time = NULL, censor_indicator = NULL, truncate_time = NULL){
-  # assumes that the censor indicator variable = 1 if censored
-  df.tmp <- data.frame(cbind(event_time, censor_indicator))
-  df.tmp$trunc_time <- ifelse(df.tmp$event_time < truncate_time, df.tmp$event_time, truncate_time)
-  df.tmp$trunc_event <- 0
-  df.tmp$trunc_event <- ifelse((df.tmp$censor_indicator == 1) & (df.tmp$event_time <= df.tmp$trunc_time), 1, df.tmp$trunc_event)
-  return(df.tmp)
-}
-
 # Perform stepwise backwards regression by p-value
 stepwise_backwards <- function(dataset, outcome,
                                glm_family="gaussian",
